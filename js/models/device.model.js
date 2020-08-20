00941eb4d911d55b2693bd23e33fd4ca38a2d70e
Sebastian Golian <sebastiangolian@gmail.com>
2020-08-20 10:51:03 +0200

    update models

--- js/models/device.model.js
+++ js/models/device.model.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Device = void 0;
 class Device {
     constructor(name, price) {
         this.name = name;
--- js/models/language.model.js
+++ js/models/language.model.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Language = void 0;
 class Language {
     constructor(id, name, level) {
         this.id = id;
